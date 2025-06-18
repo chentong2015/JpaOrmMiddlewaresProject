1aaad5d1ed3bb61455f1a193eb1c71c0f71526e4
chent5 <chen2015tong@gmail.com>
2025-06-18 16:33:41 +0200

    update criteria query example

--- jpa-queries-design/src/main/java/query_criteria/JpaCriteriaQuery.java
+++ jpa-queries-design/src/main/java/query_criteria/JpaCriteriaQuery.java
@@ -3,6 +3,7 @@ package query_criteria;
 import entity.Sample;
 import helper.EntityManagerHandler;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.FlushModeType;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -12,24 +13,35 @@ import java.util.List;
 
 public class JpaCriteriaQuery {
 
+    // TODO. 使用CriteriaQuery来构建复杂SQL
+    // select distinct top (?) s1_0.id, s1_0.name
+    // from t_sample_entity s1_0
+    // where s1_0.name=?
+    // order by s1_0.name asc
     public static void main(String[] args) {
         EntityManager entityManager = EntityManagerHandler.getEntityManager();
+
+        // TODO. 配置查询的参数条件
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Sample> criteriaQuery = criteriaBuilder.createQuery(Sample.class);
-
         Root<Sample> root = criteriaQuery.from(Sample.class);
-        criteriaQuery.select(root.get("id"));
+        criteriaQuery.distinct(true); // 必须和Order By同时使用
+        criteriaQuery.select(root);      // 查询表的所有字段(Entity)
         criteriaQuery.where(criteriaBuilder.equal(root.get("name"), "java"));
-        criteriaQuery.distinct(true);
-        // criteriaQuery.having(predicate..);
+        criteriaQuery.orderBy(criteriaBuilder.asc(root.get("name")));
+        // criteriaQuery.having();
 
+        // TODO. 配置Query相关的属性
         TypedQuery<Sample> typedQuery = entityManager.createQuery(criteriaQuery);
-        List<Sample> sampleList = typedQuery.getResultList();
-        // typedQuery.setLockMode(LockModeType.OPTIMISTIC) 没有transaction则不需要设置锁模式
+        // typedQuery.setLockMode(LockModeType.OPTIMISTIC);
+        typedQuery.setFlushMode(FlushModeType.COMMIT);
+        typedQuery.setMaxResults(100);
 
+        // 获取查询的结果
+        List<Sample> sampleList = typedQuery.getResultList();
         entityManager.close();
         for (Sample sample : sampleList) {
-            System.out.println(sample.getId());
+            System.out.println(sample);
         }
     }
 }
--- jpa-queries-design/src/main/java/query_criteria/JpaCriteriaQuerySession.java
+++ jpa-queries-design/src/main/java/query_criteria/JpaCriteriaQuerySession.java
@@ -13,67 +13,59 @@ import entity.BaseEntity;
 
 import java.util.List;
 
-public class JpaCriteriaQueryFull {
+public class JpaCriteriaQuerySession {
 
     public static void main(String[] args) {
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
         SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
         Session session = sessionFactory.openSession();
-        testSelectLabel(session);
+        testCriteriaQuerySimple(session);
         session.close();
     }
 
-    // select * from t_entity_sample;
-    private static void testSelectEntityClass(Session session) {
+    // 创建条件查询 select * from t_entity_sample;
+    private static void testCriteriaQuerySimple(Session session) {
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
         CriteriaQuery<BaseEntity> criteriaQuery = criteriaBuilder.createQuery(BaseEntity.class);
         Root<BaseEntity> root = criteriaQuery.from(BaseEntity.class);
-
-        // 获取整个Entity Class的所有属性(获取表的所有列)
         criteriaQuery.select(root);
+
         List<BaseEntity> baseEntities = session.createQuery(criteriaQuery).getResultList();
         for (BaseEntity baseEntity : baseEntities) {
             System.out.println(baseEntity);
         }
     }
 
-    // 完整的对应到SQL原生查询语句的条件
+    // TODO. 定义查询字符和多个匹配条件
     // Select distinct label from t_base_entity
     // where id>=2 and id<10
     // group by label
     // order by label desc;
-    private static void testSelectLabel(Session session) {
-        // Builder用来设置一些匹配条件
+    private static void testCriteriaQueryFull(Session session) {
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        // 创建条件查询，参数是查询结果的类型 Params: resultClass – type of the query result
         CriteriaQuery<String> criteriaQuery = criteriaBuilder.createQuery(String.class);
-        // 从指定的Entity Class中查询数据 Params: entityClass – the entity class
         Root<BaseEntity> root = criteriaQuery.from(BaseEntity.class);
 
         criteriaQuery.distinct(true);
-
-        // 执行多个属性的查询, 定义查询出来的结果class
         criteriaQuery.select(root.get("label"));
         // criteriaQuery.multiselect(root.get("id"), root.get("label"));
-
-        // 同时添加多个where的匹配条件: like, between, isNull, >=, <=
-        criteriaQuery.where(criteriaBuilder.greaterThanOrEqualTo(root.get("id"), 2),
-                criteriaBuilder.lessThan(root.get("id"), 10));
-
-        // Group By写在Order by的前面
+        criteriaQuery.where(
+                criteriaBuilder.greaterThanOrEqualTo(root.get("id"), 2),
+                criteriaBuilder.lessThan(root.get("id"), 10),
+                criteriaBuilder.isNotEmpty(root.get("label")));
         criteriaQuery.groupBy(root.get("label"));
-
-        // Hibernate API提供的两种Order By的写法
-        // criteriaQuery.orderBy(criteriaBuilder.desc(root.get("label")));
-        // criteriaQuery.orderBy(new OrderImpl(root.get("label"), false));
-
-        // having对查询出来的结果进行处理，放在最后
+        criteriaQuery.orderBy(criteriaBuilder.desc(root.get("label")));
         // criteriaQuery.having()
 
-        // 执行查询，获取返回的结果 .list()
+        // 执行Query并返回结果
         List<String> labels = session.createQuery(criteriaQuery).getResultList();
         for (String label : labels) {
             System.out.println(label);
         }
+    }
+
+    // TODO. 定义多表JOIN聚合的条件查询
+    private static void testCriteriaQueryJoin(Session session) {
+
     }
 }
