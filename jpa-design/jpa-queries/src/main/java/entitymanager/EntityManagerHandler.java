a55e52fc57a0ad8f0fb2ced94cc50d248811c735
chent5 <chen2015tong@gmail.com>
2025-10-02 19:26:19 +0200

    update jpa design

---
+++ jpa-design/jpa-queries/src/main/java/entitymanager/EntityManagerHandler.java
@@ -0,0 +1,34 @@
+package entitymanager;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EntityManagerHandler {
+
+    private static EntityManagerFactory emf;
+
+    public static EntityManager getEntityManager() {
+        emf = Persistence.createEntityManagerFactory("jpa.queries");
+        return emf.createEntityManager();
+    }
+
+    // TODO. 在代码层面动态设置DataSource并创建EntityManager对象
+    public static EntityManager getEntityManagerWithDataSource(DataSource dataSource) {
+        Map<String, Object> jpaProperties = new HashMap<>();
+        jpaProperties.put("hibernate.connection.datasource", dataSource);
+        jpaProperties.put("hibernate.dialect_resolvers", "xxxDialectResolver.class.getName()");
+        jpaProperties.put("hibernate.show_sql", "false");
+        jpaProperties.put("hibernate.hbm2ddl.auto", "none");
+        emf = Persistence.createEntityManagerFactory("name", jpaProperties);
+        return emf.createEntityManager();
+    }
+
+    public static void close() {
+        emf.close();
+    }
+}
---
+++ jpa-design/jpa-queries/src/main/java/entitymanager/query_criteria/CriteriaQueryHibernate.java
@@ -0,0 +1,68 @@
+package entitymanager.query_criteria;
+
+import entitymanager.entity.BaseEntity;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CriteriaQueryHibernate {
+
+    // TODO. Hibernate Session依赖于[hibernate.cfg.xml]配置文件
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        testCriteriaQueryFull(session);
+        session.close();
+    }
+
+    // select * from t_base_entity;
+    private static void testCriteriaQuerySimple(Session session) {
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<BaseEntity> criteriaQuery = criteriaBuilder.createQuery(BaseEntity.class);
+        Root<BaseEntity> root = criteriaQuery.from(BaseEntity.class);
+        criteriaQuery.select(root);
+
+        List<BaseEntity> baseEntities = session.createQuery(criteriaQuery).getResultList();
+        for (BaseEntity baseEntity : baseEntities) {
+            System.out.println(baseEntity);
+        }
+    }
+
+    // TODO. 定义查询字符和多个匹配条件
+    // Select distinct label from t_base_entity
+    // where id>=2 and id<10 and count in(?,?,?)
+    // group by label
+    // order by label desc;
+    private static void testCriteriaQueryFull(Session session) {
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<String> criteriaQuery = criteriaBuilder.createQuery(String.class);
+        Root<BaseEntity> root = criteriaQuery.from(BaseEntity.class);
+
+        criteriaQuery.distinct(true);
+        criteriaQuery.select(root.get("label"));
+        // criteriaQuery.multiselect(root.get("id"), root.get("label"));
+
+        criteriaQuery.where(
+                criteriaBuilder.greaterThanOrEqualTo(root.get("id"), 2),
+                criteriaBuilder.lessThan(root.get("id"), 10),
+                criteriaBuilder.isTrue(root.get("count").in(Arrays.asList(1, 2, 3))));
+        criteriaQuery.groupBy(root.get("label"));
+        criteriaQuery.orderBy(criteriaBuilder.desc(root.get("label")));
+        // criteriaQuery.having()
+
+        // 执行Query并返回结果
+        List<String> labels = session.createQuery(criteriaQuery).getResultList();
+        for (String label : labels) {
+            System.out.println(label);
+        }
+    }
+}
